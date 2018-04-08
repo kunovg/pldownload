373f777deeffc5d2ffcc4da1c522942826d91445
kunovg <kunovg12@gmail.com>
2018-04-07 22:21:17 -0500

    fixed Scrapper

--- linkgenerators/onlinevideoconverter.py
+++ linkgenerators/onlinevideoconverter.py
@@ -1,7 +1,6 @@
 import requests
 from io import StringIO
 from lxml import etree
-from lxml.html import parse
 from linkgenerators.base import Scrapper
 
 class Ovc(Scrapper):
@@ -16,21 +15,25 @@ class Ovc(Scrapper):
         r = s.post("https://www2.onlinevideoconverter.com/webservice",
             data={
                 'function': 'validate',
-                'args[urlEntryUser]': 'https://www.youtube.com/watch?v=%s' % self.idvideo,
+                'args[urlEntryUser]': 'https://www.youtube.com/watch?v=%s' % "SNHQW5P3l4A",
                 'args[fromConvert]': 'urlconverter',
                 'args[requestExt]': 'mp3',
                 'args[nbRetry]': '0',
+                'args[videoResolution]': '-1',
                 'args[audioBitrate]': '0',
                 'args[audioFrequency]': '0',
                 'args[channel]': 'stereo',
                 'args[volume]': '0',
                 'args[startFrom]': '-1',
                 'args[endTo]': '-1',
+                'args[custom_resx]': '-1',
+                'args[custom_resy]': '-1',
+                'args[advSettings]': 'false',
+                'args[aspectRatio]': '-1',
             }
         )
-        # print(r.json())
+
         r = s.get('https://www.onlinevideoconverter.com/es/success?id=%s' % r.json()['result']['dPageId'])
-        # print(r.)
         parser = etree.HTMLParser()
         tree = etree.parse(StringIO(r.text), parser)
         return {'link': tree.xpath('//a[@id="downloadq"]')[0].attrib['href']}
